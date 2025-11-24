e5b6d65173d67aa8a21be1ae52e6badc30d8791c
hanseo-lab <hanseo.lab@gmail.com>
2025-11-24 20:13:09 +0900

    Initial commit

---
+++ src/main/java/com/kh/tia/TiaApplication.java
@@ -0,0 +1,13 @@
+package com.kh.tia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/tia/TiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.tia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
